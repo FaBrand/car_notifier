645c257083486eb5201af45e6728e822c5562a60
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2018-12-26 17:07:54 +0100

    Add navbar component
    Change-Id: I3d04d6e81490f61267998a8809de3b8975180683

--- frontend/car-notifier-app/src/app/app.module.ts
+++ frontend/car-notifier-app/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarsComponent } from './cars/cars.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent
+    CarsComponent,
+    NavbarComponent
   ],
   imports: [
+      NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
