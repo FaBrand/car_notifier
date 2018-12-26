d39194fadfe9310a3ecbb015f516af3ad1171e84
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2018-12-26 15:45:57 +0100

    Add basic cars component
    Change-Id: I4fc4fdc5c4ea3dfa3db20bbfc8f837d634ecaba1

--- frontend/car-notifier-app/src/app/app-routing.module.ts
+++ frontend/car-notifier-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarsComponent } from './cars/cars.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'cars', component: CarsComponent },
+    { path: '', redirectTo: 'cars', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
